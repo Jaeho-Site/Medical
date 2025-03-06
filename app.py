c57c992e9a8328990321e07e809d9baa281acd8e
Jaeho <sjaeho348@gamil.com>
2025-03-06 23:00:29 +0900

    feat : JavaScript알약식별함수 구현
    -index.html에 css, js 구현

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from waitress import serve
 
 app = Flask(__name__)
@@ -6,6 +6,21 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/identify', methods=['POST'])
+def identify_pill():
+    if 'image' not in request.files:
+        return jsonify({'error': '이미지가 업로드되지 않았습니다.'}), 400
+    
+    return jsonify({
+        'pill_info': {
+            'shape': '원형',
+            'color': '흰색',
+            'imprint': '테스트'
+        },
+        'identified_pills': ['테스트 약품'],
+        'result_html': '<table id="idfytotal0"><tr><th>테스트</th></tr><tr><td>테스트 데이터</td></tr></table>'
+    })
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8085)
